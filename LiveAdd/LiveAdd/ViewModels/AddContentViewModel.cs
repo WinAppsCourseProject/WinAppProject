8abe4be843eafb555a6abeaeeccb7ba7f2995424
TsvetanRazsolkov <tsvetan.razsolkov@gmail.com>
2015-12-19 14:05:49 +0200

    Loading data from parse into Main page

--- LiveAdd/LiveAdd/ViewModels/AddContentViewModel.cs
+++ LiveAdd/LiveAdd/ViewModels/AddContentViewModel.cs
@@ -44,7 +44,6 @@
                 double latitude = 0.0;
                 double longitude = 0.0;
 
-
                 var accessStatus = await Geolocator.RequestAccessAsync();
                 if (accessStatus != GeolocationAccessStatus.Allowed)
                 {
--- LiveAdd/LiveAdd/ViewModels/MainPageViewModel.cs
+++ LiveAdd/LiveAdd/ViewModels/MainPageViewModel.cs
@@ -5,17 +5,25 @@
     using System.Collections.Generic;
     using System.Collections.ObjectModel;
     using System.Linq;
+    using Windows.Devices.Geolocation;
 
     using Extensions;
     using System.Threading.Tasks;
     using Models;
+    using Helpers;
+
     public class MainPageViewModel : ViewModelBase
     {
-        public ObservableCollection<AddViewModel> advertisements;
+        private const int EarthRadius = 6371;
+        private const int MaximumDistanceInKilometers = 50;
+
+        private static Geolocator geolocator = new Geolocator();
+
+        private ObservableCollection<AddViewModel> advertisements;
 
         public MainPageViewModel()
         {
-            //this.LoadAddsAsync();
+            this.LoadAddsAsync();
         }
 
         public IEnumerable<AddViewModel> Advertisements
@@ -54,11 +62,39 @@
 
         private async void LoadAddsAsync()
         {
+            double latitude = 0.0;
+            double longitude = 0.0;
+
+            var accessStatus = await Geolocator.RequestAccessAsync();
+            if (accessStatus != GeolocationAccessStatus.Allowed)
+            {
+                Notifier.ShowNotification("Unauthorized access to location.");
+            }
+            else
+            {
+                Geoposition geoposition = await geolocator.GetGeopositionAsync();
+                latitude = geoposition.Coordinate.Point.Position.Latitude;
+                longitude = geoposition.Coordinate.Point.Position.Longitude;
+            }
+
             var ads = await new ParseQuery<AddModel>()
-                .Where(x => true)
-                .FindAsync();
+            .Where(a => this.CheckAvailableAds(a, latitude, longitude))
+            .FindAsync();
 
             this.Advertisements = ads.AsQueryable().Select(AddViewModel.FromModel);
+        }
+
+        private bool CheckAvailableAds(AddModel model, double latitude, double longitude)
+        {
+            var parseLatitude = model.Location.Latitude;
+            var parseLongitude = model.Location.Longitude;
+
+            var dlat = (latitude - parseLatitude) / 180 * Math.PI;
+            var dlon = (longitude - parseLongitude) / 180 * Math.PI;
+            var x = (dlon) * Math.Cos((latitude + parseLatitude) / 2 / 180 * Math.PI);
+            var d = Math.Sqrt(x * x + dlat * dlat) * EarthRadius;
+
+            return model.IsActive == true && d <= MaximumDistanceInKilometers;
         }
     }
 }
