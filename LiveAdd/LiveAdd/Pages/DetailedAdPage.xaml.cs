0568d410386e32582c35a1cbca1f4f22d7dc855d
VenelinGP <venelin.petkov@netcoms.eu>
2015-12-20 17:19:58 +0200

    Created DetailedAdPage

--- LiveAdd/LiveAdd/Pages/DetailedAdPage.xaml.cs
+++ LiveAdd/LiveAdd/Pages/DetailedAdPage.xaml.cs
@@ -1,17 +1,7 @@
 ﻿using LiveAdd.ViewModels;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
+using Parse;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
 // The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
@@ -47,6 +37,21 @@ namespace LiveAdd.Pages
             base.OnNavigatedTo(e);
 
             this.ViewModel = e.Parameter as AddViewModel;
+        }
+
+        private void btnMenuPanel_Click(object sender, RoutedEventArgs e)
+        {
+            MenuPanel.IsPaneOpen = !MenuPanel.IsPaneOpen;
+        }
+
+        private void MenuPanelView_CreateNewAdv(object sender, System.EventArgs e)
+        {
+            this.Frame.Navigate(typeof(AddContentPage));
+        }
+        private void MenuPanelView_LogOut(object sender, System.EventArgs e)
+        {
+            ParseUser.LogOut();
+            this.Frame.Navigate(typeof(LoginPage));
         }
     }
 }
